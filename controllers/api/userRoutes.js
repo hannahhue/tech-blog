a41a2732ee343c726feda9bae5831640cb5d179c
unknown <alicia9790972@gmail.com>
2022-10-26 21:03:15 -0400

    fixed username bug

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,16 +3,12 @@ const { User } = require("../../models");
 
 router.post("/", async (req, res) => {
   try {
-    const dbUserData = await User.create({
-      username: req.body.username,
-      password: req.body.password,
-    });
+    const dbUserData = await User.create(req.body);
 
     req.session.save(() => {
       req.session.loggedIn = true;
-
-      res.status(200).json(dbUserData);
     });
+    res.status(200).json(dbUserData);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
